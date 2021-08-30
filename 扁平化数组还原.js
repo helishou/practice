567ac4e6972822a19ee62929435bc4f76fab10e8
helishou <479525390@qq.com>
2021-08-30 11:30:50 +0800

    111

--- 扁平化数组还原.js
+++ 扁平化数组还原.js
@@ -1,34 +1,27 @@
 /*
  * @Author       : helishou
  * @Date         : 2021-08-23 17:04:39
- * @LastEditTime : 2021-08-23 17:06:04
+ * @LastEditTime : 2021-08-30 11:30:08
  * @LastEditors  : helishou
  * @Description  :
- * @FilePath     : \插曲-测试\扁平化数组还原.js
+ * @FilePath     : \扁平化数组还原.js
  * 你用你的指尖,阻止我说再见,在bug完全失去之前
  */
 var data = [
-  { name: 'ABC', parent: 'DEF', relation: 'ghi', depth: 1 },
-  { name: 'DEF', parent: 'null', relation: 'null', depth: 0 },
-  { name: 'new_name', parent: 'ABC', relation: 'rel', depth: 2 },
-  { name: 'a_name', parent: 'null', relation: 'rel', depth: 2 },
+  { name: "ABC", parent: "DEF", relation: "ghi", depth: 1 },
+  { name: "DEF", parent: "null", relation: "null", depth: 0 },
+  { name: "new_name", parent: "ABC", relation: "rel", depth: 2 },
+  { name: "a_name", parent: "null", relation: "rel", depth: 2 },
 ];
 var dataMap = {};
-data.forEach(function (node) {
-  dataMap[node.name] = node;
-});
-console.log(dataMap, 'dataMap');
+data.map((node) => (dataMap[node.name] = node));
+console.log(dataMap, "dataMap");
 var tree = [];
 data.forEach(function (node) {
-  // find parent
   var parent = dataMap[node.parent];
   if (parent) {
-    // create child array if it doesn't exist
-    (parent.children || (parent.children = []))
-      // add node to parent's child array
-      .push(node);
+    (parent.children || (parent.children = [])).push(node);
   } else {
-    // parent is null or missing
     tree.push(node);
   }
 });
